579f19f966ba401eac96a58a255dba3ef1127658
MariaGeorge22 <142611912+MariaGeorge22@users.noreply.github.com>
2024-01-18 20:43:10 +0200

    opcodes.c

--- push_opcode.c
+++ push_opcode.c
@@ -19,6 +19,34 @@ void push(stack_t **stack, unsigned int line_number)
 }
 
 /**
+ * pop - monty opcode - remove value in the stack
+ * @stack: stack to update the node or the array
+ * @line_number: number of instruction line
+ *
+ * executes the pop opcode
+ *
+ * Return: void
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_current = NULL;
+
+	(void)line_number;
+	if ((*stack) == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->next != NULL)
+	{
+		new_current = (*stack)->next;
+		new_current->prev = NULL;
+	}
+	free((*stack));
+	(*stack) = new_current;
+}
+
+/**
  * pall - monty opcode - prints values in the stack
  * @stack: stack to update the node or the array
  * @line_number: number of instruction line
@@ -37,4 +65,53 @@ void pall(stack_t **stack, unsigned int line_number)
 		printf("%d\n", current->n);
 		current = current->next;
 	}
+}
+
+/**
+ * pint - monty opcode
+ * @stack: stack to update
+ * @line_number: number of instruction line
+ *
+ * executes the pint
+ *
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	(void)line_number;
+	if (current == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", current->n);
+}
+/**
+ * swap - monty opcode
+ * @stack: stack to update
+ * @line_number: number of instruction line
+ *
+ * executes the swap
+ *
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp, *top;
+
+	(void)line_number;
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	top = (*stack)->next;
+	temp = (*stack)->next->next;
+	top->next = (*stack);
+	top->prev = NULL;
+	(*stack)->next = temp;
+	(*stack)->prev = top;
+	(*stack) = top;
 }
